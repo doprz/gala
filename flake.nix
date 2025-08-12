5927ae62fc050844dd0c467da353a2ca62a55a57
doprz <52579214+doprz@users.noreply.github.com>
2025-08-12 17:02:57 -0500

    feat(nix): add nix flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs =
+    inputs:
+    let
+      goVersion = 24;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # go (version is specified by overlay)
+              go
+
+              # goimports, godoc, etc.
+              gotools
+
+              # https://github.com/golangci/golangci-lint
+              golangci-lint
+            ];
+          };
+        }
+      );
+    };
+}
